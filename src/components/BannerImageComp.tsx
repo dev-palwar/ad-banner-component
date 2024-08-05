21bbaedbcdefc0372794976712d2992b3a4b43ef
Dev Palwar <devpalwar06@gmail.com>
2024-08-05 15:34:33 +0530

    added a loader

--- src/components/BannerImageComp.tsx
+++ src/components/BannerImageComp.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Ad from "./Ad";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { loadCSSModule } from "@/utils/cssLoader";
+import Loader from "./loader";
 
 export interface AdProps {
   id: number;
@@ -14,14 +15,20 @@ export interface AdProps {
 
 const BannerImageComp: React.FC<AdProps> = (props) => {
   const [styles, setStyles] = React.useState<any>({});
+  const [loading, setLoading] = React.useState<boolean>(true);
 
   React.useEffect(() => {
     const loadStyles = async () => {
       const loadedStyles = await loadCSSModule(props.id);
       setStyles(loadedStyles);
+      setLoading(false);
     };
     loadStyles();
   }, [props.id]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <React.Fragment>
